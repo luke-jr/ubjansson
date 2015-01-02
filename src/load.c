bcb5777f11c66d37fc9f2003264edbe21604ce5a
Luke Dashjr <luke-jr+git@utopios.org>
2015-01-02 12:10:12 +0000

    load: Add missing copyright for copied code

--- src/load.c
+++ src/load.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015 Luke Dashjr <luke-jr+jansson@utopios.org>
+ * Copyright (c) 2009-2012 Petri Lehtinen <petri@digip.org>
  *
  * Jansson is free software; you can redistribute it and/or modify
  * it under the terms of the MIT license. See LICENSE for details.
